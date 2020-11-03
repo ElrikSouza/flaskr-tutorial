f88a5b0347fb81c9bccbd427765b5fa7a37e0f20
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2020-11-03 14:40:23 -0400

    add

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -10,43 +10,6 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 
 
-@bp.route('/')
-def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
-        ' FROM post p JOIN user u ON p.author_id = u.id'
-        ' ORDER BY created DESC'
-    ).fetchall()
-
-    return render_template('blog/index.html', posts=posts)
-
-
-@bp.route('/create', methods=('GET', 'POST'))
-@login_required
-def create():
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'INSERT INTO post (title, body, author_id) VALUES (?,?,?)',
-                (title, body, g.user['id'])
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
-
-    return render_template('blog/create.html')
-
-
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -64,37 +27,81 @@ def get_post(id, check_author=True):
     return post
 
 
-@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@bp.route('/')
+def index():
+    db = get_db()
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+
+    return render_template('blog/index.html', posts=posts)
+
+
+@bp.route('/create', methods=('POST',))
 @login_required
-def update(id):
+def save_post():
+    title = request.form['title']
+    body = request.form['body']
+    error = None
+
+    if not title:
+        error = 'Title is required'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'INSERT INTO post (title, body, author_id) VALUES (?,?,?)',
+            (title, body, g.user['id'])
+        )
+        db.commit()
+        return redirect(url_for('blog.index'))
+
+
+@bp.route("/create")
+@login_required
+def create_post_page():
+    return render_template('blog/create.html')
+
+
+@bp.route('/<int:id>/update')
+@login_required
+def update_post_page(id):
     post = get_post(id)
-
-    if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
-        error = None
-
-        if not title:
-            error = 'Title is required.'
-
-        if error is not None:
-            flash(error)
-        else:
-            db = get_db()
-            db.execute(
-                'UPDATE post SET title = ?, body = ?'
-                ' WHERE id = ?',
-                (title, body, id)
-            )
-            db.commit()
-            return redirect(url_for('blog.index'))
 
     return render_template('blog/update.html', post=post)
 
 
+@bp.route('/<int:id>/update', methods=('POST',))
+@login_required
+def update_post(id):
+    title = request.form['title']
+    body = request.form['body']
+    error = None
+
+    if not title:
+        error = 'Title is required.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute(
+            'UPDATE post SET title = ?, body = ?'
+            ' WHERE id = ?',
+            (title, body, id)
+        )
+        db.commit()
+
+        return redirect(url_for('blog.index'))
+
+
 @bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
-def delete(id):
+def delete_post(id):
     get_post(id)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
